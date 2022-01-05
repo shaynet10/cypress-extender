65404dc1acad98b69fa67f467795dcb30c0746a8
Shay Guueta <shaynet10@gmail.com>
2022-01-06 00:08:51 +0200

    Adding more options to functions of loggers

--- src/logger.js
+++ src/logger.js
@@ -11,10 +11,18 @@ export const Info = (message, ...args) => log("INFO", message, ...args);
 export const Message = (message, ...args) => log("MESSAGE", message, ...args);
 export const Debug = (message, ...args) => log("DEBUG", message, ...args);
 export const Warn = (message, ...args) => log("WARN", message, ...args);
+export const INFO = (message, ...args) => log("INFO", message, ...args);
+export const MESSAGE = (message, ...args) => log("MESSAGE", message, ...args);
+export const DEBUG = (message, ...args) => log("DEBUG", message, ...args);
+export const WARN = (message, ...args) => log("WARN", message, ...args);
+export const Custom = (header, message, ...args) => log(header, message, ...args);
+export const CUSTOM = (header, message, ...args) => log(header, message, ...args);
+
 
 export const initCypressWithLogger = () => {
     Cypress.Commands.add('Info', Info);
     Cypress.Commands.add('Message', Message);
     Cypress.Commands.add('Debug', Debug);
     Cypress.Commands.add('Warn', Warn);
+    Cypress.Commands.add('Custom', Custom);
 };
