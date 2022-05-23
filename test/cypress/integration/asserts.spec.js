acbf18994c131be020257601a165848042daaff8
Shay Gueta <shaynet10@gmail.com>
2022-05-23 23:50:50 +0300

    adding test files

---
+++ test/cypress/integration/asserts.spec.js
@@ -0,0 +1,9 @@
+describe('asserts', () => {
+    beforeEach(() => {
+        cy.visit('https://www.google.com/');
+    });
+    
+    it('some test', () => {
+        console.log('some test');
+    });
+});
